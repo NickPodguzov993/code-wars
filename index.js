d2e507de5ec593a8f95c610a9e717e40478020bf
NickPodguzov993 <verytallman993@gmail.com>
2023-07-10 12:28:20 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function strCount(str, letter){
+    //code here
+}
+
+let str = 'assdf'
+let letter = 's'
+let strArray = str.split('')
+let count = 0
+
+console.log(strArray)
+
+
+console.log(strArray.filter(f=>f === letter).length)
+
